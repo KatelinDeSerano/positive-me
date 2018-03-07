1dca8bab105db40340de01caaaf841b48c4f2783
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-03-06 22:47:21 -0600

    Log out working

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,24 @@
 import React from "react";
 import './nav.css';
 import {Link} from 'react-router-dom';
+import { clearAuth } from "../actions/auth";
+import {connect} from "react-redux";
 
-export default function Nav() {
-    return (
+export class Nav extends React.Component {
+    logout() {
+        this.props.dispatch(clearAuth());
+    }
+    render() {
+      return (
         <div>
             <ul className="nav">
                 <li><Link to="/dashboard">PositiveMe</ Link></li>
                 <li><Link to="/journal_history">Journal History</ Link></li>
-                <li className="logout"> Log Out</li>
+                <li className="logout" onClick={() => this.logout()}> Log Out</li>
             </ul>
         </div>
-    );
+      );
+    }
 };
+
+export default connect()(Nav);
