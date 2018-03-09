46f8f50f2790dfa166f745002e6bd25c2ea3652f
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-03-08 21:58:11 -0600

    chart working

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,90 +1,105 @@
 import React from "react";
 import './chart.css';
+import {connect} from "react-redux";
 var LineChart = require('react-chartjs-2').Line;
+var BarChart = require('react-chartjs-2').Bar;
 
-export default function Chart(props) {
-    
-    var chartData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    };
-    const chartOptions = {
+export class Chart extends React.Component {
+    render() {
+        let labels = this.props.journal.map(entry => {
+            return entry.date; 
+        });
+        let negative = this.props.journal.map(entry => {
+            return entry.negativeFeeling; 
+        });
+        let positive = this.props.journal.map(entry => {
+            return entry.positiveFeeling; 
+        });
+        var chartData = {
+            labels: labels,
+            datasets: [
+                {
+                    label: "My Negative Feelings",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: negative
+                },
+                {
+                    label: "My Positive Feelings",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: positive
+                }
+            ]
+        };
+        const chartOptions = {
 
-        ///Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines : false,
-    
-        //String - Colour of the grid lines
-        scaleGridLineColor : "rgba(0,0,0,.05)",
-    
-        //Number - Width of the grid lines
-        scaleGridLineWidth : 1,
-    
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
-    
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines: true,
-    
-        //Boolean - Whether the line is curved between points
-        bezierCurve : true,
-    
-        //Number - Tension of the bezier curve between points
-        bezierCurveTension : 0.4,
-    
-        //Boolean - Whether to show a dot for each point
-        pointDot : true,
-    
-        //Number - Radius of each point dot in pixels
-        pointDotRadius : 4,
-    
-        //Number - Pixel width of point dot stroke
-        pointDotStrokeWidth : 1,
-    
-        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-        pointHitDetectionRadius : 20,
-    
-        //Boolean - Whether to show a stroke for datasets
-        datasetStroke : true,
-    
-        //Number - Pixel width of dataset stroke
-        datasetStrokeWidth : 2,
-    
-        //Boolean - Whether to fill the dataset with a colour
-        datasetFill : true,
+            ///Boolean - Whether grid lines are shown across the chart
+            scaleShowGridLines : false,
         
-        //String - A legend template
-        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>",
-       
-        //Boolean - Whether to horizontally center the label and point dot inside the grid
-        offsetGridLines : false
-    };
-    
-    return (
-        <div className="chart">
-        <LineChart data={chartData} options={chartOptions} width={400} height={250} />
+            //String - Colour of the grid lines
+            scaleGridLineColor : "rgba(0,0,0,.05)",
         
-        </div>
-    );
+            //Number - Width of the grid lines
+            scaleGridLineWidth : 1,
+        
+            //Boolean - Whether to show horizontal lines (except X axis)
+            scaleShowHorizontalLines: true,
+        
+            //Boolean - Whether to show vertical lines (except Y axis)
+            scaleShowVerticalLines: true,
+        
+            //Boolean - Whether the line is curved between points
+            bezierCurve : true,
+        
+            //Number - Tension of the bezier curve between points
+            bezierCurveTension : 0.4,
+        
+            //Boolean - Whether to show a dot for each point
+            pointDot : true,
+        
+            //Number - Radius of each point dot in pixels
+            pointDotRadius : 4,
+        
+            //Number - Pixel width of point dot stroke
+            pointDotStrokeWidth : 1,
+        
+            //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+            pointHitDetectionRadius : 20,
+        
+            //Boolean - Whether to show a stroke for datasets
+            datasetStroke : true,
+        
+            //Number - Pixel width of dataset stroke
+            datasetStrokeWidth : 2,
+        
+            //Boolean - Whether to fill the dataset with a colour
+            datasetFill : true,
+            
+            //String - A legend template
+            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>",
+        
+            //Boolean - Whether to horizontally center the label and point dot inside the grid
+            offsetGridLines : false
+        };
+        
+        return (
+            <div className="chart">
+                <BarChart data={chartData} options={chartOptions} width={400} height={250} />
+            </div>
+        );
+    }
 };
 
+const mapStateToProps = state => ({
+    journal: state.positiveReducer.journal
+});
+export default connect(mapStateToProps)(Chart);
--- src/reducers/positiveReducer.js
+++ src/reducers/positiveReducer.js
@@ -6,12 +6,30 @@ const initialState = {
     journal: [
       {
         negativeThought: "I'm terrible at this.",
-        negativeFeeling: "Sad",
+        negativeFeeling: -2,
         negativeEvidence: "It's terrible.",
         alternativeEvidence: "It's not really terrible, it works.",
         positiveThought: "I can improve on this,  I can practice and get better.",
-        positiveFeeling: "Happy",
+        positiveFeeling: 2,
         date: "1/1/2018"
+      },
+      {
+        negativeThought: "I'm terrible at this.",
+        negativeFeeling: -1,
+        negativeEvidence: "It's terrible.",
+        alternativeEvidence: "It's not really terrible, it works.",
+        positiveThought: "I can improve on this,  I can practice and get better.",
+        positiveFeeling: 1,
+        date: "1/2/2018"
+      },
+      {
+        negativeThought: "I'm terrible at this.",
+        negativeFeeling: -2,
+        negativeEvidence: "It's terrible.",
+        alternativeEvidence: "It's not really terrible, it works.",
+        positiveThought: "I can improve on this,  I can practice and get better.",
+        positiveFeeling: 0,
+        date: "1/3/2018"
       }
     ]
 };
