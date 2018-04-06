5503b69457bce214210beec0cd870a5251929a1d
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-04-05 21:30:59 -0500

    Basic structure of list and detail view functionality

--- src/components/journalDetail.js
+++ src/components/journalDetail.js
@@ -1,44 +1,33 @@
 import React from "react";
 import './journalDetail.css';
-
+import {connect} from "react-redux";
 import coolFace from '../images/260102-emoji/svg/cool-1.svg';
 import crying from '../images/260102-emoji/svg/crying-2.svg';
 import sad from '../images/260102-emoji/svg/sad-2.svg';
 import neutral from '../images/260102-emoji/svg/confused.svg';
 import happy from '../images/260102-emoji/svg/happy-2.svg';
 
-export default class AddressBook extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            journal: {
-                negativeThought: "I'm terrible at this.",
-                negativeFeeling: 1,
-                negativeEvidence: "It's terrible.",
-                alternativeEvidence: "It's not really terrible, it works.",
-                positiveThought: "I can improve on this,  I can practice and get better.",
-                positiveFeeling: 4,
-                date: "1/1/2018"
-            }
-        };
-
-        this.emoji = [
-            crying,
-            sad,
-            neutral,
-            happy,
-            coolFace  
-        ];
-
-    }
+export class JournalDetail extends React.Component {
     
     render() {
         console.log(this.state);
 
         console.log(this.emoji);
-        // const journal = this.state.map((journal, index) =>
-        //     <AddressBook key={index} index={index} {...journal} />
-        // );  
+        let negativeThought = this.props.journal.map(entry =>  {
+            return entry.negativeThought;
+        }); 
+        let negativeFeeling = this.props.journal.map(entry =>  {
+            return entry.emojiValue1;
+        });
+        let evidenceAgainstThought = this.props.journal.map(entry =>  {
+            return entry.evidenceAgainstThought;
+        });
+        let positiveThought = this.props.journal.map(entry =>  {
+            return entry.positiveThought;
+        });
+        let positiveFeeling = this.props.journal.map(entry =>  {
+            return entry.emojiValue2;
+        }); 
         return (
             <div className="journalDetail">
                 <div className="journalDetailMenu">
@@ -47,21 +36,21 @@ export default class AddressBook extends React.Component {
                     <i className="far fa-edit" id="icon"></i>
                 </div> 
                 <div className="journalContent">
-                <h3>{this.state.journal.date}</h3>
+                <h3>Date</h3>
                 <ul>
                 <li>Describe a negative thought or thoughts you had today:</li>
-                <p>{this.state.journal.negativeThought}</p>
+                <p>{negativeThought}</p>
                 <li>How does that thought make you feel?</li>
-                <img src={this.emoji[this.state.journal.negativeFeeling]} 
+                <img src={negativeFeeling} 
                     className="emoji" />
-                <li>Is there substantial evidence for my thought?</li>
-                <p>{this.state.journal.negativeEvidence}</p>
+                {/* <li>Is there substantial evidence for my thought?</li>
+                <p>{this.state.journal.negativeEvidence}</p> */}
                 <li>Is there evidence contrary to my thought?</li>
-                <p>{this.state.journal.alternativeEvidence}</p>
+                <p>{evidenceAgainstThought}</p>
                 <li>What would a positive alternative thought be in this instance?</li>
-                <p>{this.state.journal.positiveThought}</p>
+                <p>{positiveThought}</p>
                 <li>How does the postive thought make you feel?</li>
-                <img src={this.emoji[this.state.journal.positiveFeeling]} 
+                <img src={positiveFeeling} 
                     className="emoji" />
                 </ul>
                 </div>
@@ -70,3 +59,7 @@ export default class AddressBook extends React.Component {
     }
 }
 
+const mapStateToProps = state => ({
+    journal: state.positiveReducer.journal
+})
+export default connect(mapStateToProps)(JournalDetail);
--- src/components/journalListView.js
+++ src/components/journalListView.js
@@ -1,18 +1,35 @@
 import React from "react";
 import './journalListView.css';
+import {connect} from "react-redux";
 
-export default function JournalListView(props) {
-    return (
-        <div className="journalListView">
-            <div className="listViewMenu">
-                <i className="fas fa-plus" id="icon"></i>
-                <i className="fas fa-trash-alt" id="icon"></i> 
-                <i className="far fa-edit" id="icon"></i>
-            
-            <div className="journalDate">
-            <h3>MM.DD.YYYY</h3>  
-            </div>
-            </div> 
-        </div>
-    );
+export class JournalListView extends React.Component {
+    
+    render () {
+        const journalEntries = this.props.journal.map((entry, index) => (
+            <li key={index}>{entry.date}</li>
+        ));
+        return ( 
+            <ul>
+                {journalEntries}
+            </ul>
+        );
+    };
 };
+
+const mapStateToProps = state => ({
+    journal: state.positiveReducer.journal
+})
+export default connect(mapStateToProps)(JournalListView);
+
+
+{/* <div className="journalListView">
+                <div className="listViewMenu">
+                    <i className="fas fa-plus" id="icon"></i>
+                    <i className="fas fa-trash-alt" id="icon"></i> 
+                    <i className="far fa-edit" id="icon"></i>
+                
+                <div className="journalDate">
+                <h3>MM.DD.YYYY</h3>  
+                </div>
+                </div> 
+            </div> */}
